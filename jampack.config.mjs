81c4487213ea7b7be9d95914d22e0d44d75d4db9
LBA <macbook@Macbooks-MacBook-Pro.local>
2024-10-12 02:19:21 +0100

    fix og meta

--- jampack.config.mjs
+++ jampack.config.mjs
@@ -2,5 +2,8 @@ export default {
     image: {
         srcset_min_width: 100,
         srcset_step: 100,
+        compress: true,
+        rcset_max_width: 1280,  // Max width used in srcset
+        max_width: 720, 
     },
 };
