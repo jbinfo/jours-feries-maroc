2f1b5865ed51e47e03c4a15ed3d719fe1d32d6fe
LBA <macbook@Macbooks-MacBook-Pro.local>
2024-10-16 12:44:08 +0100

    optimize

--- js/js-year-calendar.fr.js
+++ js/js-year-calendar.fr.js
@@ -1,11 +1,9 @@
-window.onload = function () {
-	Calendar.locales['fr'] = {
-		days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
-		daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
-		daysMin: ["D", "L", "Ma", "Me", "J", "V", "S"],
-		months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
-		monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Déc"],
-		weekShort:'S',
-		weekStart: 1
-	};
-}
+Calendar.locales['fr'] = {
+	days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+	daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+	daysMin: ["D", "L", "Ma", "Me", "J", "V", "S"],
+	months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+	monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Déc"],
+	weekShort:'S',
+	weekStart: 1
+};
